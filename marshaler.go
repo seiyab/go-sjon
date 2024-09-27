a788e403ff891f346d4e50d7850bcfed3d40ba8e
seiya <20365512+seiyab@users.noreply.github.com>
2024-09-27 22:30:21 +0900

    TextMarshaler

--- marshaler.go
+++ marshaler.go
@@ -1,30 +1,71 @@
 package sjon
 
 import (
+	"encoding"
 	"encoding/json"
+	"io"
 	"reflect"
 
 	"github.com/pkg/errors"
 )
 
-var jsonMarshalerType = reflect.TypeOf((*json.Marshaler)(nil)).Elem()
+var (
+	jsonMarshalerType = reflect.TypeOf((*json.Marshaler)(nil)).Elem()
+	textMarshalerType = reflect.TypeOf((*encoding.TextMarshaler)(nil)).Elem()
+)
 
-func shouldMarshalWithMethod(v reflect.Value) bool {
-	t := v.Type()
-	return t.Implements(jsonMarshalerType) ||
-		reflect.PointerTo(t).Implements(jsonMarshalerType)
+func marshalWithMarshalJSON(v reflect.Value, out io.Writer) (bool, error) {
+	return marshalWithMethod(v, out, func(v json.Marshaler) ([]byte, error) {
+		return v.MarshalJSON()
+	})
 }
 
-func marshalWithMethod(v reflect.Value) ([]byte, error) {
+func marshalWithMarshalText(v reflect.Value, out io.Writer) (bool, error) {
+	return marshalWithMethod(v, out, func(v encoding.TextMarshaler) ([]byte, error) {
+		b, err := v.MarshalText()
+		if err != nil {
+			return nil, err
+		}
+		q := make([]byte, 0, len(b)+2)
+		q = append(q, '"')
+		q = append(q, b...)
+		q = append(q, '"')
+		return q, nil
+	})
+}
+
+func marshalWithMethod[T any](
+	v reflect.Value,
+	out io.Writer,
+	method func(v T) ([]byte, error),
+) (bool, error) {
+	marshalerType := reflect.TypeOf((*T)(nil)).Elem()
+	t := v.Type()
+	if !(t.Implements(marshalerType) ||
+		reflect.PointerTo(t).Implements(marshalerType)) {
+		return false, nil
+	}
 	if v.Kind() == reflect.Ptr {
 		if v.IsNil() {
-			return []byte("null"), nil
+			_, err := out.Write([]byte("null"))
+			if err != nil {
+				return false, err
+			}
+			return true, nil
 		}
 		v = v.Elem()
 	}
-	m, ok := v.Interface().(json.Marshaler)
+	m, ok := v.Interface().(T)
 	if !ok {
-		return nil, errors.Errorf("go-json: internal error: %v does not implement json.Marshaler", v.Type())
+		return false, errors.Errorf("go-json: internal error: %v does not implement Marshaler", v.Type())
 	}
-	return m.MarshalJSON()
+	b, err := method(m)
+	if err != nil {
+		return false, err
+	}
+	_, err = out.Write(b)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
